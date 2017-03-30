49c63938f16792c59fa940470efa1933603a2705
rufer7 <marc.rufer@d-fens.net>
2017-03-30 18:05:28 +0200

    Initialise Client
    * Created Net.Appclusive.Api project in Net.Appclusive.Net.Client repository
    * Created Net.Appclusive.Net.Client project in Net.Appclusive.Net.Client repository
    * Created Test projects for Net.Appclusive.Api and Net.Appclusive.Net.Client
    
    dfensgmbh/Net.Appclusive/78

---
+++ src/CommonAssemblySettings.cs
@@ -0,0 +1,34 @@
+﻿/**
+ * Copyright 2017 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription(".Net Client for the Appclusive Blueprint Modelling and Automation Engine")]
+[assembly: AssemblyCompany("d-fens GmbH")]
+[assembly: AssemblyCopyright("Copyright © 2013-2017 d-fens GmbH")]
+[assembly: AssemblyTrademark("The d-fens logo is a registered trademark in the European Union and/or other countries. Appclusive is a registered trademark in Switzerland and/or other countries.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("4.0.0.0")]
---
+++ src/Net.Appclusive.Net.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿/**
+ * Copyright 2017 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Net.Appclusive.Net.Client")]
+[assembly: AssemblyProduct("Net.Appclusive.Net.Client")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("41b8af8e-4de1-4c98-a581-932b4d6daa1f")]
+
+[assembly: InternalsVisibleTo("Net.Appclusive.Net.Client.Tests")]
